1a11f11aaa512e99901b821548366ce06cf4135b
nekitech <nekitech-x@mail.ru>
2025-02-11 12:34:57 +0300

    feat: google sheets api

--- packages/gsheets/connect.ts
+++ packages/gsheets/connect.ts
@@ -1,12 +1,6 @@
-import path from 'node:path';
-import { config } from 'dotenv';
 import { JWT } from 'google-auth-library';
-import { GoogleSpreadsheet } from 'google-spreadsheet';
 import creds from './creds.json';
-
-config({
-	path: path.resolve(process.cwd(), '.env'),
-});
+import { Excel } from './src/excel';
 
 const serviceAccountAuth = new JWT({
 	email: creds.client_email,
@@ -14,20 +8,14 @@ const serviceAccountAuth = new JWT({
 	scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
-//
-// const sheet = doc.sheetsByTitle.pages_cast;
-// await sheet?.loadHeaderRow(2);
-//
-// const data = await sheet?.getRows();
-// const jsonData = data?.map(row => row.toObject());
-
 export async function connectGoogleApiTable() {
 	try {
-		const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEETS_TABLE_ID ?? '', serviceAccountAuth);
-		await doc.loadInfo();
-		console.log(`Connection is success! Amount of pages is: ${doc.sheetCount}`);
-		return doc;
+		const gsh = new Excel(serviceAccountAuth);
+		await gsh.init();
+		console.log(await gsh.getRowsBySheetName('Структура данных', 1));
 	} catch (e: any) {
 		throw new Error(e.message);
 	}
 }
+
+await connectGoogleApiTable();
---
+++ packages/gsheets/src/excel.ts
@@ -0,0 +1,67 @@
+import path from 'node:path';
+import { config } from 'dotenv';
+import { JWT } from 'google-auth-library';
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+
+export class Excel {
+	table: GoogleSpreadsheet | null = null;
+
+	constructor(serviceAccountAuth: JWT) {
+		try {
+			config({
+				path: path.resolve(process.cwd(), '../.env'),
+			});
+			this.table = new GoogleSpreadsheet(process.env.GOOGLE_SHEETS_TABLE_ID ?? '', serviceAccountAuth);
+		} catch (e: any) {
+			throw new Error(e.message);
+		}
+	}
+
+	async init() {
+		await this.table?.loadInfo();
+	}
+
+	async getNameSheets() {
+		return this.table?.sheetsByIndex.map(sheet => sheet.title);
+	}
+
+	async addRow(sheetName: string, rowData: any) {
+		const sheet = this.table?.sheetsByTitle[sheetName];
+		if (!sheet) throw new Error(`Лист '${sheetName}' не найден`);
+
+		await sheet.addRow(rowData);
+		console.log(`Строка добавлена в лист '${sheetName}':`, rowData);
+	}
+
+	async getRowsBySheetName(sheetName: string, header = 2) {
+		const sheet = this.table?.sheetsByTitle[sheetName];
+		if (!sheet) throw new Error(`Лист '${sheetName}' не найден`);
+		await sheet?.loadHeaderRow(header);
+
+		const data = await sheet?.getRows();
+		return data?.map(row => row.toObject());
+	}
+
+	async deleteSheet(sheetName: string) {
+		const sheet = this.table?.sheetsByTitle[sheetName];
+		if (!sheet) throw new Error(`Лист '${sheetName}' не найден`);
+
+		await sheet.delete();
+		console.log(`Лист '${sheetName}' удалён`);
+	}
+
+	async deleteRow(sheetName: string, rowIndex: number) {
+		const sheet = this.table?.sheetsByTitle[sheetName];
+		if (!sheet) throw new Error(`Лист '${sheetName}' не найден`);
+
+		await sheet.loadCells();
+		const rows = await sheet.getRows();
+
+		if (rowIndex < 0 || rowIndex >= rows.length) {
+			throw new Error(`Строка с индексом ${rowIndex} не найдена`);
+		}
+
+		await rows[rowIndex]?.delete();
+		console.log(`Строка ${rowIndex} удалена из листа '${sheetName}'`);
+	}
+}
